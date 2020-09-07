2bdf3474d9391d178425b650861df828f857ba9d
Chi-Chuan <chichuan.chen46@gmail.com>
2020-09-07 11:40:02 +0800

    Added correlation analyses using Hmisc package

--- beh_brain_corr.R
+++ beh_brain_corr.R
@@ -18,16 +18,38 @@ exclude_subj_ID <- c(306, 309, 311, 312, 321, 326, 335, 339, 351, 341, 348, 354,
 
 beh_final <- beh_raw %>%
   filter(!is.na(Date), !ID %in% exclude_subj_ID) %>%
-  select(Date, ID, Age, stimQ, ends_with("PR")) %>%
+  select(Date, ID, Age, stimQ, ends_with("PR"), Homeread) %>%
   arrange(Date)
+
+
 
 
 # beh brain combined dataframe
 beh_omi_HbO_all_channels <- beh_final %>% bind_cols(omission_HbO)
 
-# channel of interest for all subjs: B5 = 7th channel
-corr_data <- beh_omi_HbO_all_channels %>% select(X7, stimQ, ends_with("PR"))
-install.packages("jpeg")
-install.packages("latticeExtra")
+
+
 install.packages("Hmisc")
 library("Hmisc")
+
+
+# channel of interest for all subjs: B5 = 7th channel
+corr_data <- beh_omi_HbO_all_channels %>% select(X7, stimQ, ends_with("PR"), Homeread)
+
+corr_pearson_allsubj <- rcorr(as.matrix(corr_data), type = "pearson")
+corr_spearman_allsubj <- rcorr(as.matrix(corr_data), type = "spearman")
+
+# channel of interest for 16 months: B5 = 7th channel, D7 = 13th channel
+corr_data_gp16 <- beh_omi_HbO_all_channels %>% filter(Age=="16M") %>% select(X7, X13, stimQ, ends_with("PR"), Homeread)
+
+corr_pearson_gp16 <- rcorr(as.matrix(corr_data_gp16), type = "pearson")
+corr_spearman_gp16 <- rcorr(as.matrix(corr_data_gp16), type = "spearman")
+
+# channel of interest for 19, 20 months: E7 = 14th channel
+corr_data_gp1920 <- beh_omi_HbO_all_channels %>% 
+  filter(Age %in% c("19M", "20M")) %>% select(X14, stimQ, ends_with("PR"), Homeread)
+
+corr_pearson_gp1920 <- rcorr(as.matrix(corr_data_gp1920), type = "pearson")
+corr_spearman_gp1920 <- rcorr(as.matrix(corr_data_gp1920), type = "spearman")
+
+
